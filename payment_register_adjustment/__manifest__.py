d8494721d6fc8a1167bfc8c917224824d1c739f0
MarwaZad <marwa.abozad@gmail.com>
2025-08-27 13:34:53 +0300

    payment_register_adjustment

---
+++ payment_register_adjustment/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Register Payment Adjustment",
+    'summary': "Register Payment Adjustment",
+
+
+    'author': "Marwa Abouzaid",
+
+    # for the full list
+    'category': 'account',
+    'version': '16.0.0',
+    "license": "AGPL-3",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ payment_register_adjustment/models/model.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager
+from odoo import models
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+
+    @contextmanager
+    def _check_balanced(self, container):
+        threshold = 50  # 👈 فرق مسموح به
+        try:
+             if self._context.get('active_model') == 'account.move':
+                for move in self:
+                    # print('moveeeeeeeeeeeee', move.id)
+                    # print('payment_difference_handling', move.payment_id.payment_difference_handling)
+                    # print('payment_difference', move.payment_id.payment_difference)
+                    # if move.payment_id and move.payment_id.payment_difference_handling == "reconcile":
+                    debit = sum(line.debit for line in move.line_ids)
+                    credit = sum(line.credit for line in move.line_ids)
+                    print('debit', debit)
+                    print('credit', credit)
+
+                    if abs(debit - credit) <= threshold:
+                        _logger.warning(">>> SKIPPING BALANCE CHECK (diff=%.2f) <<<", debit - credit)
+                        yield
+                        return
+        except Exception as e:
+            _logger.error(">>> ERROR in custom _check_balanced: %s", e)
+
+        # fallback → السلوك العادي
+        with super(AccountMove, self)._check_balanced(container):
+            yield
