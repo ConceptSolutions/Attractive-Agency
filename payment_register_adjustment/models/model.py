4ea815c9d8be8bf6956812dbcb19d94d5b71a271
MarwaZad <marwa.abozad@gmail.com>
2025-12-19 09:48:04 +0200

    payment_register_adjustment

--- payment_register_adjustment/models/model.py
+++ payment_register_adjustment/models/model.py
@@ -1,6 +1,8 @@
 from contextlib import contextmanager
 from odoo import models
 import logging
+from contextlib import contextmanager
+from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
 
@@ -10,26 +12,25 @@ class AccountMove(models.Model):
 
     @contextmanager
     def _check_balanced(self, container):
-        threshold = 50  # 👈 فرق مسموح به
-        try:
-             if self._context.get('active_model') == 'account.move':
-                for move in self:
-                    
-                    # print('payment_difference_handling', move.payment_id.payment_difference_handling)
-                    # print('payment_difference', move.payment_id.payment_difference)
-                    # if move.payment_id and move.payment_id.payment_difference_handling == "reconcile":
-                    debit = sum(line.debit for line in move.line_ids)
-                    credit = sum(line.credit for line in move.line_ids)
-                    print('debit', debit)
-                    print('credit', credit)
+        threshold = 50
+        skip_check = False
 
-                    if abs(debit - credit) <= threshold:
-                        _logger.warning(">>> SKIPPING BALANCE CHECK (diff=%.2f) <<<", debit - credit)
-                        yield
-                        return
-        except Exception as e:
-            _logger.error(">>> ERROR in custom _check_balanced: %s", e)
+        if self._context.get('active_model') == 'account.move':
+            for move in self:
+                debit = sum(move.line_ids.mapped('debit'))
+                credit = sum(move.line_ids.mapped('credit'))
 
-        # fallback → السلوك العادي
-        with super(AccountMove, self)._check_balanced(container):
+                diff = abs(debit - credit)
+                if diff <= threshold:
+                    skip_check = True
+                    _logger.warning(
+                        "SKIP BALANCE CHECK move=%s diff=%.2f",
+                        move.name, diff
+                    )
+                    break
+
+        if skip_check:
             yield
+        else:
+            with super(AccountMove, self)._check_balanced(container):
+                yield
